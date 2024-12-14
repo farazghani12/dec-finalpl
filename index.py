9ffdf60f10a84a9af04010d2aaad6adf0eb09342
farazghani12 <farazahmadghani@outlook.com>
2024-12-14 22:55:52 +0500

    change in index file

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)  # Creates a Flask application instance.
 
 @app.route('/')  # Defines a route for the root URL ('/').
 def hello_world():  # Function that handles requests to the root URL.
-    return 'Hello, World!'  # Returns a response.
+    return 'Hello, World from docker!'  # Returns a response.
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
